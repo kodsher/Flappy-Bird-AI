c7c989983a28e011c06028962594607569ac688b
kodsher <kodsher@users.noreply.github.com>
2024-07-16 22:29:52 -0500

    Update bird.js

--- bird.js
+++ bird.js
@@ -15,7 +15,7 @@ class Bird {
     // position and size of bird
     this.x = 64;
     this.y = height / 2;
-    this.r = 12;
+    this.r = 120;
 
     // Gravity, lift, velocity.
     this.gravity = 0.8;
